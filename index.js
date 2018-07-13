5f7cfa76c0f9276c7ea5c3edbdeb6216aab5b490
Andrew Sprague <andrewsprague@outlook.com>
2018-07-13 14:51:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {
+  Apple: 'Apples'
+};
+
+function updateObjectWithKeyAndValue (obj, key, i) {
+  obj[key] = i;
+  return obj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
+  obj[key] = value;
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey (obj, key) {
+  delete obj[key];
+  return obj
+}
+
+function deleteFromObjectByKey (obj, key) {
+  
+}
